718b1b1b4cee0002523307e8209132015598db99
coutbiaafnts <bia.fontes653@gmail.com>
2024-09-16 21:36:01 -0300

    feat: aula 05 e 06 - variáveis e omentários

---
+++ módulo-01/aula-05/assets/js/script.js
@@ -0,0 +1,9 @@
+let age = 90 /* number */
+let price = 0.33 // number
+let name = 'Bianca' /* string */
+let login = true /* boolean */
+let address = ['rua', 'cidade', 'estado'] /* array (object) */
+let fullName = { name: 'Bianca', age: 21 } /* objeto */
+
+/* mostrando o tipo da variável */
+console.log(typeof fullName)
