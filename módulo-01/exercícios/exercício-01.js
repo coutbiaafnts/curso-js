81bd3846d2128287193dc68ca9ac75625a51f59b
coutbiaafnts <bia.fontes653@gmail.com>
2024-09-16 21:42:29 -0300

    feat: exercício 01

---
+++ módulo-01/exercícios/exercício-01.js
@@ -0,0 +1,11 @@
+/* 1. Crie uma variável para armazenar o nome do carro e ponha o valor Ferrari. Logo após, exiba o nome */
+let carName = 'Ferrari'
+console.log(carName)
+
+/* 2. Crie uma variável para armazenar o preço do bolo, que custa R$19,99. Logo após, exiba o preço */
+let cakePrice = 19.99
+console.log(cakePrice)
+
+/* 3. Corrija o código a seguir e exiba o resultado (let "cidade" = São Paulo) */
+let cidade = 'São Paulo'
+console.log(cidade)
